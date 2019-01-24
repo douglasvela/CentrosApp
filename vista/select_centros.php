9031efd1318716193ad1a1911c105257b4c2902b
Unknown <d_recinos@outlook.com>
2019-01-24 08:26:35 -0600

    efwefewfw

--- vista/select_centros.php
+++ vista/select_centros.php
@@ -1,6 +1,6 @@
 <div class="row"><div class="col-lg-12 col-md-4">
                             <div class="form-group">
-                                <select class="select2" style="width:100%" name="id_centro" id="id_centro" onchange="cargar_indicadores(this.value)">
+                                <select class="select2" style="width:95%" name="id_centro" id="id_centro" onchange="cargar_indicadores(this.value)">
                                 <?php
                                 $conexion = mysqli_connect("162.241.252.245","proyedk4_WPZF0","MAYO_nesa94","proyedk4_WPZF0"); 
                                     $query_consulta_centros=mysqli_query($conexion,"select * from cdr_centro"); 
