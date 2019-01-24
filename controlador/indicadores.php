92a0e3ded5e8f46aeff3921ac05e426bcbdd8e92
Unknown <d_recinos@outlook.com>
2019-01-24 08:07:08 -0600

    dsvsdvsdvds

--- controlador/indicadores.php
+++ controlador/indicadores.php
@@ -28,26 +28,7 @@ $id_centro = $_POST['id_centro'];
 	
 ?>
 <div class="container-fluid"> <br>
-    <div class="row"><div class="col-lg-12 col-md-4">
-                            <div class="form-group">
-                                <select class="select2" style="width:100%" name="id_centro" id="id_centro" onchange="cargar_indicadores(this.value)">
-                                <?php
-                                    $query_consulta_centros=mysqli_query($conexion,"select * from cdr_centro"); 
-                                    while( $fila_centros=mysqli_fetch_array($query_consulta_centros)){
-                                            $indicador_centros[] = $fila_centros;
-                                            }
-                                    foreach ($indicador_centros as $indicador_centros_fila) {
-                                        
-                                        
-                                    ?>
-                                        <option value="<?php echo $indicador_centros_fila[0];?>"><?php echo $indicador_centros_fila[1];?></option>
-                                    <?php
-                                        }
-                                    ?>
-                            </select>
-                            </div>
-                        </div>
-                        </div>
+    
 	<div class="row">       	
 		<div class="col-lg-4 col-md-4">
                 <div class="card card-inverse card-warning" onclick="">
